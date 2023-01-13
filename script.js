f74f47776501f0ab045b72b5c64e93ca4995800d
Iryna Fedorova <i.fedorova4321@gmailcom>
2023-01-14 00:33:04 +0200

    Homework 18: modified

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-// // // // Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+// Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
 
 const arr = [222, 30, 50, 'a', 2, 'c', 'f', 1, 't'];
 
@@ -17,47 +17,55 @@ function calculateNum(arr) {
 const result = calculateNum(arr)
 alert(result);
 
-// // // // Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
+// Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
 const x = +prompt('Введіть x');
 const y = +prompt('Введіть y');
 const znak = prompt('Введіть знак(+, -, *, /, %, ^)');
 
 function doMath(x, znak, y) {
-    if (!x || !y || !znak) {
-        return 'Ви нічого не ввели!'
-    } else if (isNaN(x) || isNaN(y) || znak.length !== 1) {
-        return 'Не можу порахувати!'
+    if (!x && x !== 0 || !y && y !== 0 || !znak) {
+        return 'Ви нічого не ввели або ввели невалідні дані!'
+    } else {
+        switch (znak) {
+            case '+':
+                return (x + y)
+            case '-':
+                return (x - y)
+            case '*':
+                return (x * y)
+            case '/':
+                return (x / y)
+            case '%':
+                return (x % y)
+            case '^':
+                return (x ** y)
+            default:
+                return 'Не знаю такий знак'
+        }
     }
-    return eval(x + znak.replace('^', '**') + y)
 }
 
 const newResult = doMath(x, znak, y);
 alert(newResult)
 
-// // // // Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
+// Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
 function userArr() {
     let array = [];
+
     const mainArr = +prompt("Введіть кількість массивів в головному масиві");
 
-    if (!mainArr) {
+    if (!mainArr || mainArr < 0) {
         return 'error'
     }
 
     for (let i = 0; i < mainArr; i++) {
-        array.push([]);
-        const subArr = prompt(`Введіть кількість елементів у ${i + 1} масиві`);
-        if (!subArr) {
+        const subArr = prompt(`Введіть через пробіл елементи у ${i + 1} масиві`).split(' ');
+        if (!subArr || subArr < 0) {
             return 'error'
         }
-        for (let j = 0; j < subArr; j++) {
-            const element = prompt(`Введіть текст ${j + 1} елементу в масиві ${i + 1}`);
-            if (!element) {
-                return 'error'
-            }
-            array[i].push([element]);
-        }
+        array[i] = subArr
     }
     return array
 }
@@ -65,7 +73,7 @@ function userArr() {
 const userData = userArr();
 alert(userData);
 
-// // Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
+// Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
 const str = prompt('Введіть рядок');
 const toRemove = prompt('Введіть через пробіл два символи, які б ви хотіли прибрати');
@@ -85,4 +93,3 @@ function removeEl(str, toRemove) {
 
 const removed = removeEl(str, toRemove)
 alert(removed)
-
